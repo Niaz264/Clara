5cda8df28d4b4ec664c6a1352a35548f8d8508ec
Niaz264 <82967650+Niaz264@users.noreply.github.com>
2022-03-10 23:55:45 +0600

    Update and rename N to config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -14,17 +14,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
+    API_ID = 7232694  # integer value, dont use ""
+    API_HASH = "959d9ed64a5fb41ba3418248ff1fd835"
+    TOKEN = "5016945112:AAE0JoU0Z94GpaGPzSuglqrPpvdIzHbWpAM"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1538405771  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "ribochan"
     SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001764710818
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001799411291
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
